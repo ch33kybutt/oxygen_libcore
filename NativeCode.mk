79a44848bcbb39474864610cab59d0fc170ae722
Sachin Kamat <sachin.kamat@linaro.org>
2011-03-15 15:46:26 +0530

    Disable Werror option to avoid build errors using GCC 4.5/4.6.
     Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    
    Change-Id: I2c4699686ada362b43b836ace61636fc000720d3

--- NativeCode.mk
+++ NativeCode.mk
@@ -85,7 +85,7 @@ core_static_libraries := $(sort $(LOCAL_STATIC_LIBRARIES))
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS += -Wall -Wextra -Werror
+LOCAL_CFLAGS += -Wall -Wextra
 
 ifeq ($(TARGET_ARCH),arm)
 # Ignore "note: the mangling of 'va_list' has changed in GCC 4.4"
